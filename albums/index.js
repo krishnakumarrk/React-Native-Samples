6c5895b98df4f07b617a4767be6b042db9105eeb
krishnaitsmyn <=krishnaitsmyn@gmail.com>
2018-01-21 08:30:52 -0600

    ReactJs Udemy Project

---
+++ albums/index.js
@@ -0,0 +1,11 @@
+// Import library to create a component
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+
+//Create a component
+const App = () => (
+    <Text> Test Text </Text>
+  );
+
+//Render it to the device
+AppRegistry.registerComponent('albums', () => App);
