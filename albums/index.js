b4bed4339c6deeae644813f7937a5fb49912bc8f
krishnaitsmyn <=krishnaitsmyn@gmail.com>
2018-02-03 12:55:48 -0600

    Changes for new components

--- albums/index.js
+++ albums/index.js
@@ -1,11 +1,16 @@
 // Import library to create a component
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 //Create a component
 const App = () => (
-    <Text> Test Text </Text>
+  <View>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
   );
 
 //Render it to the device
-AppRegistry.registerComponent('albums', () => App); 
+AppRegistry.registerComponent('albums', () => App);
